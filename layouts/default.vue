0dc44782ef01d2e3db03d7a1c989d2e67f85ce08
vitalijalbu <40239902+vitalijalbu@users.noreply.github.com>
2018-10-11 12:21:19 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,23 +6,23 @@
   <b-collapse is-nav id="nav_collapse">
 
         <b-navbar-nav class="ml-auto">
-	<b-nav-item href="destinations">destinations</b-nav-item>
-		<b-nav-item href="resorts">resorts</b-nav-item>
-	 <b-nav-item href="offers">offers</b-nav-item>
-	 <b-nav-item href="help">help</b-nav-item>
+	<b-nav-item href="/destinations">Destinations</b-nav-item>
+		<b-nav-item href="/search">Search</b-nav-item>
+	 <b-nav-item href="/offers">Offers</b-nav-item>
+	 <b-nav-item href="/help">Help?</b-nav-item>
 	 <b-nav-item href="checkout">Cart</b-nav-item>
 	 <b-nav-item-dropdown text="Hi, user" right>
-        <b-dropdown-item href="user/profile">profile</b-dropdown-item>
-        <b-dropdown-item href="user/bookings">bookings</b-dropdown-item>
-        <b-dropdown-item href="user/saved">saved</b-dropdown-item>
-        <b-dropdown-item href="user/settings">account</b-dropdown-item>
-        <b-dropdown-item href="<?php echo base_url();?>manager/dashboard">Manager centro</b-dropdown-item>
+        <b-dropdown-item href="/user/profile">My profile</b-dropdown-item>
+        <b-dropdown-item href="/user/bookings">My bookings</b-dropdown-item>
+        <b-dropdown-item href="/user/saved">Saved</b-dropdown-item>
+        <b-dropdown-item href="/user/settings">My account</b-dropdown-item>
+        <b-dropdown-item href="/manager/dashboard">Manager centro</b-dropdown-item>
         <b-dropdown-divider></b-dropdown-divider>
         <b-dropdown-item href="user/logout">logout</b-dropdown-item>
       </b-nav-item-dropdown>
 
-		<b-nav-item @click="flip('signin', $event)" href="#signin">login</b-nav-item>
-		<b-nav-item @click="flip('signup', $event)" href="#signup">signup</b-nav-item>
+		<b-nav-item @click="flip('signin', $event)" href="#signin">Login</b-nav-item>
+		<b-nav-item @click="flip('signup', $event)" href="#signup">Signup</b-nav-item>
     </b-navbar-nav>
   <nuxt/>
 </div>
