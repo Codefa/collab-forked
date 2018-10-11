52badb1cd98a40530cb39c04aac6d34295ea0f6c
vitalijalbu <40239902+vitalijalbu@users.noreply.github.com>
2018-10-11 12:19:51 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
-  <b-navbar toggleable>
-    <b-navbar-toggle target="nav_dropdown_collapse"></b-navbar-toggle>
-    <b-collapse is-nav id="nav_dropdown_collapse">
-      <b-navbar-nav>
-        <b-nav-item href="/">Home</b-nav-item>
-        <b-nav-item href="/search">Search</b-nav-item>
-        <!-- Navbar dropdowns -->
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-        <b-nav-item-dropdown text="User" right>
-          <b-dropdown-item href="/user/profile">Account</b-dropdown-item>
-          <b-dropdown-item href="#">Settings</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
+ <b-navbar toggleable="md" type="light" fixed="top">
+  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+  <b-navbar-brand href="/"><img class="nav-logo" src="assets/img/logo.svg" alt="Sauspa logo"><b-badge variant="primary">BETA</b-badge></b-navbar-brand>
+  <b-collapse is-nav id="nav_collapse">
+
+        <b-navbar-nav class="ml-auto">
+	<b-nav-item href="destinations">destinations</b-nav-item>
+		<b-nav-item href="resorts">resorts</b-nav-item>
+	 <b-nav-item href="offers">offers</b-nav-item>
+	 <b-nav-item href="help">help</b-nav-item>
+	 <b-nav-item href="checkout">Cart</b-nav-item>
+	 <b-nav-item-dropdown text="Hi, user" right>
+        <b-dropdown-item href="user/profile">profile</b-dropdown-item>
+        <b-dropdown-item href="user/bookings">bookings</b-dropdown-item>
+        <b-dropdown-item href="user/saved">saved</b-dropdown-item>
+        <b-dropdown-item href="user/settings">account</b-dropdown-item>
+        <b-dropdown-item href="<?php echo base_url();?>manager/dashboard">Manager centro</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item href="user/logout">logout</b-dropdown-item>
+      </b-nav-item-dropdown>
+
+		<b-nav-item @click="flip('signin', $event)" href="#signin">login</b-nav-item>
+		<b-nav-item @click="flip('signup', $event)" href="#signup">signup</b-nav-item>
+    </b-navbar-nav>
   <nuxt/>
 </div>
   
