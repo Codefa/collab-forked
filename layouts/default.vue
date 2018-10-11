fd0899042d3cc2fd7803d12aa5c9e677f9eac72e
vitalijalbu <40239902+vitalijalbu@users.noreply.github.com>
2018-10-11 10:09:46 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,8 +4,8 @@
     <b-navbar-toggle target="nav_dropdown_collapse"></b-navbar-toggle>
     <b-collapse is-nav id="nav_dropdown_collapse">
       <b-navbar-nav>
-        <b-nav-item href="#">Home</b-nav-item>
-        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#/">Home</b-nav-item>
+        <b-nav-item href="/search">Search</b-nav-item>
         <!-- Navbar dropdowns -->
         <b-nav-item-dropdown text="Lang" right>
           <b-dropdown-item href="#">EN</b-dropdown-item>
@@ -14,7 +14,7 @@
           <b-dropdown-item href="#">FA</b-dropdown-item>
         </b-nav-item-dropdown>
         <b-nav-item-dropdown text="User" right>
-          <b-dropdown-item href="#">Account</b-dropdown-item>
+          <b-dropdown-item href="/user/profile">Account</b-dropdown-item>
           <b-dropdown-item href="#">Settings</b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
