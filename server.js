787baab79722538f85b243fd49cca8245ee26775
SHARON <sharushj690@gmail.com>
2018-10-13 21:05:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express')
-const serveStatic = require("serve-static")
 const path = require('path')
-app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
-const port = process.env.PORT || 80
-app.listen(port)
+const history = require('connect-history-api-fallback')
+
+const app = express()
+
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
+
+app.use(staticFileMiddleware)
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}))
+app.use(staticFileMiddleware)
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'))
+})
+
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port
+  console.log('App now running on port', port)
+})
