31b7a4436e89f6466c3551aab613cb39687e81c8
SHARON <sharushj690@gmail.com>
2018-11-06 18:08:14 +0530

    update

--- src/main.js
+++ src/main.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import { store } from './store'
 import BootstrapVue from 'bootstrap-vue'
-import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
 import * as VueGoogleMaps from 'vue2-google-maps'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'vuetify/dist/vuetify.min.css'
 import AirbnbStyleDatepicker from 'vue-airbnb-style-datepicker'
 import 'vue-airbnb-style-datepicker/dist/vue-airbnb-style-datepicker.min.css'
 import NavBar from './components/Includes/NavBar'
@@ -24,7 +22,6 @@ const datepickerOptions = {}
 
 Vue.use(Vuex)
 Vue.use(BootstrapVue)
-Vue.use(Vuetify)
 Vue.use(VueGoogleMaps, {
   load: {
     key: 'AIzaSyCe1exctmeJjIb4guyT6newSpyJ7kA3aLc',
